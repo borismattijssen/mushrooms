c1a8884fcd81f657d5c02010783e9de623b73900
borismattijssen <borismattijssen@gmail.com>
2018-12-15 19:14:17 +0100

    Changes predict model to only use one model as input

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -10,24 +10,17 @@ import pandas as pd
 
 models_dir = os.path.join(Path(__file__).resolve().parents[2], 'models')
 
-def default_none(ctx, param, value):
-    if len(value) == 0:
-        return [
-            os.path.join(models_dir, 'knn-high-res.p'),
-            os.path.join(models_dir, 'rf-grid-search.p')
-        ]
-
 @click.command()
 @click.argument('input_filepath', nargs=1)
-@click.argument('models', nargs=-1, callback=default_none)
-def main(input_filepath, models):
+@click.argument('model', nargs=1, default=os.path.join(models_dir, 'rf-grid-search.p'))
+def main(input_filepath, model):
     """ INPUT_FILEPATH A CSV containing mushrooms. The file should contain a CSV header.\n
-    MODELS a list of classification models.
+    MODEL a filepath to a pickled classification model.
     """
     logger = logging.getLogger(__name__)
 
     # load model
-    model = find_best_model(logger, models)
+    model = pickle.load(open(model, "rb"))
 
     # load pca model and one-hot column names
     pca = pickle.load(open(os.path.join(models_dir, 'pca.p'), "rb"))
@@ -40,38 +33,6 @@ def main(input_filepath, models):
 
     classes = [str(x).replace("1", "p").replace("0", "e") for x in model.predict(pcad)]
     print("\n".join(classes))
-
-
-    # find optimal model
-
-def find_best_model(logger, models):
-    estimators = []
-    scores = []
-
-    # load all models
-    for model_path in models:
-        try:
-            (e, s) = load(model_path)
-            estimators.append(e)
-            scores.append(s)
-            logger.info('Added model {}'.format(model_path))
-        except Exception as e:
-            logger.info('Failed to add model {}'.format(model_path))
-
-    # find the index of the highest score
-    ix = scores.index(max(scores))
-
-    # return the estimator with the highest score
-    return estimators[ix]
-
-def load(model_file):
-    """ Load a model from a filepath.
-    """
-
-    # if we should load from cache and the cache file exists
-    clf = pickle.load(open(model_file, "rb"))
-    return (clf.best_estimator_, clf.best_score_)
-
 
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
